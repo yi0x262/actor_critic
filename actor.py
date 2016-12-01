77155c96a11402ee10c38445bfd159c32eaf4fd7
yi0x262 <yutadefn@gmail.com>
2016-12-01 14:27:09 +0900

    trace eligibility

--- actor.py
+++ actor.py
@@ -1,46 +1,66 @@
 #!/usr/env/bin python3
-
 import numpy as np
+from npcounter import npcounter
 #An Analysis of Actor-Critic Algorithms using Eligibility Traces:Reinforcement Learning with Imperfect Value Functions (kimura,1996?)
 #!!basic version (no eligibility traces)!!
 class actor(object):
-    def __init__(self,inout,alpha=0.05):
+    def __init__(self,inout,alpha=0.05,beta=0.9):
         """
         inout = (in,out)
         alpha = learning gain (0 < alpha)
+        beta  = discount rate (for eligibility)
         """
         self.alpha = alpha
+        self.beta = beta
 
-        Mi  = np.empty((1,inout[0]))#input
-        Mo  = np.empty((1,inout[1]))#output
+        i,o = inout
 
-        self.W_exp      = np.zeros(inout)       #
-        self.W_var      = np.ones_like(Mo)      #
+        self.W_exp      = np.zeros(inout)           #expectation
+        self.W_var      = 0.5*np.ones((1,o))        #variance
 
-        self.lastState  = np.zeros_like(Mi)     #
-        self.lastAct  = np.zeros_like(Mo)       #
+        self.D_exp      = np.zeros_like(self.W_exp) #eligibility tracer
+        self.D_var      = np.zeros_like(self.W_var)
+
+        self.lastState  = np.zeros((1,i))           #for update
+        self.lastAct    = np.zeros((1,o))
+
+        self.update_counter = npcounter(o)          #
 
     def action(self,state):
         """
         state : np.array(ndim=1) or list_like
         """
-        s = np.array([state])
-        mu    = self.mu(s)
+        self.lastState = np.array([state])
+        mu    = self.mu(self.lastState)
         sigma = self.sigma()
         self.lastAct   = np.random.normal(mu,sigma)
-        self.lastState = s
         return self.lastAct
+
     def update(self,TDerror,dt):
         """
         dW_exp/dt = alpha*TDerr*state.(Act-Mu)
         dW_var/dt = alpha*TDerr*((Act-Mu)^2 - Sigma^2)
+
+        at:act,st:state,mu:mu,sg:sigma
+        e_exp = (at - mu).st
+        e_var = ((at - mu)**2 - sg**2)(1-sg)
+
         """
         mu = self.mu(self.lastState)
         sigma = self.sigma()
+        update = self.update_counter(sigma[0],dt)
 
-        a = dt*self.alpha*TDerror
-        self.W_exp += a*np.dot(self.lastState.T,self.lastAct-mu)
-        self.W_var += a*(np.power(self.lastAct-mu,2)-np.power(sigma,2))
+        e_exp = self.lastState.T.dot(self.lastAct - mu)
+        e_var = ((self.lastAct - mu)**2 - sigma**2)*(1-sigma)
+
+        print(e_exp)
+
+        self.D_exp += update*((1-self.beta)*self.D_exp + e_exp)
+        self.D_var += update*((1-self.beta)*self.D_var + e_var)
+
+        self.W_exp += update*self.alpha*TDerror*self.D_exp
+        self.W_var += update*self.alpha*TDerror*self.D_var
+
     def mu(self,state):
         return np.dot(state,self.W_exp)
     def sigma(self):
---
+++ npcounter.py
@@ -0,0 +1,25 @@
+import numpy as np
+class npcounter(object):
+    def __init__(self,num):
+        """
+        num : num of counter
+        """
+        self.counter = np.zeros(num)
+    def __call__(self,limits,dt):
+        """
+        limits  : time limits (list or numpy ndim=1)
+        dt      : add time (float scalar or numpy ndim=1)
+        """
+        self.counter += dt               #countup
+        ret = limits < self.counter      #cmp
+        self.counter -= ret*self.counter #count reset
+        return ret
+
+if __name__ == '__main__':
+    c = npcounter(3)
+    d = [0,0,0]
+    timelist = [1,2,3]
+    dt = 0.1
+    for t in range(100):
+        d = [a+b for a,b in zip(c(timelist,dt),d)]
+        print('{0:3.2f}'.format(dt*t),c.counter,d)
